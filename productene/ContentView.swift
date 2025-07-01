d2d454d51d6683f06373b54b23e2bca1741667e4
Yasu <barutanseven@gmail.com>
2025-07-01 16:59:50 +0900

    Initial Commit

---
+++ productene/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  productene
+//
+//  Created by 田中正造 on 01/07/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ productene/producteneApp.swift
@@ -0,0 +1,17 @@
+//
+//  producteneApp.swift
+//  productene
+//
+//  Created by 田中正造 on 01/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct producteneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
